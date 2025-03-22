f253684d686b03c78010dda3c33b283176a6bc1d
Wildan Fathir Qinthara <wildanfathir99@gmail.com>
2025-03-22 22:36:07 +0700

    feat: set up project

---
+++ app/Views/css_js.php
@@ -0,0 +1,18 @@
+<link href="http://pbo_wildan/assets/css/bootstrap.min.css" rel="stylesheet" />
+<link href="http://pbo_wildan/assets/css/bootstrap-responsive.min.css" rel="stylesheet" />
+<link rel="stylesheet" href="http://pbo_wildan/assets/css/font-awesome.min.css" />
+<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300" />
+<link rel="stylesheet" href="http://pbo_wildan/assets/css/ace.min.css" />
+<link rel="stylesheet" href="http://pbo_wildan/assets/css/ace-responsive.min.css" />
+<link rel="stylesheet" href="http://pbo_wildan/assets/css/ace-skins.min.css" />
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+<script type="text/javascript">
+	window.jQuery || document.write("<script src='http://pbo_wildan/assets/js/jquery-2.0.3.min.js'>"+"<"+"/script>");
+</script>
+<script type="text/javascript">
+	if("ontouchend" in document) document.write("<script src='http://pbo_wildan/assets/js/jquery.mobile.custom.min.js'>"+"<"+"/script>");
+</script>
+<script src="http://pbo_wildan/assets/js/bootstrap.min.js"></script>
+<script src="http://pbo_wildan/assets/js/ace-elements.min.js"></script>
+<script src="http://pbo_wildan/assets/js/ace.min.js"></script>
