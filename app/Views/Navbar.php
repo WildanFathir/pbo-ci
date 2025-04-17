d95fdee8e539a28e82ebe83d22b740f9a466cc71
WildanFathir <wildanfathir612@gmail.com>
2025-04-17 18:31:27 +0700

    feat: update Dashboard, Navbar, and Sidebar views for TOKO v1

--- app/Views/Navbar.php
+++ app/Views/Navbar.php
@@ -1,13 +1,49 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>CI4</title>
-</head>
-<body>
-  <?php
+<div class="navbar-inner">
+				<div class="container-fluid">
+					<a href="#" class="brand">
+						<small>
+							<i class="icon-leaf"></i>
+							SISTEM INFORMASI TOKO
+						</small>
+					</a><!--/.brand-->
 
-  ?>
-</body>
-</html>
+					<ul class="nav ace-nav pull-right">
+						<li class="light-blue">
+							<a data-toggle="dropdown" href="#" class="dropdown-toggle">
+								<img class="nav-user-photo" src="<?php echo base_url();?>/assets/avatars/user.jpg" alt="user" />
+								<span class="user-info">
+									<small>Selamat Datang</small>
+									Wildan Fathir Qinthara
+								</span>
+
+								<i class="icon-caret-down"></i>
+							</a>
+
+							<ul class="user-menu pull-right dropdown-menu dropdown-yellow dropdown-caret dropdown-closer">
+								<li>
+									<a href="#">
+										<i class="icon-cog"></i>
+										Settings
+									</a>
+								</li>
+
+								<li>
+									<a href="#">
+										<i class="icon-user"></i>
+										Profile
+									</a>
+								</li>
+
+								<li class="divider"></li>
+
+								<li>
+									<a href="#">
+										<i class="icon-off"></i>
+										Logout
+									</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+			</div>
--- app/Views/SideBar.php
+++ app/Views/SideBar.php
@@ -1,13 +1,62 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>CI4</title>
-</head>
-<body>
-  <?php
-  
-  ?>
-</body>
-</html>
+<ul class="nav nav-list">
+					<li>
+						<a href="index.html">
+							<i class="icon-dashboard"></i>
+							<span class="menu-text"> Dashboard </span>
+						</a>
+					</li>
+
+					<li>
+						<a href="typography.html">
+							<i class="icon-text-width"></i>
+							<span class="menu-text"> Karyawan </span>
+						</a>
+					</li>
+
+					<li class="">
+						<a href="#" class="dropdown-toggle">
+							<i class="icon-file-alt"></i>
+
+							<span class="menu-text">
+								Data Produk
+								<span class="badge badge-primary ">4</span>
+							</span>
+
+							<b class="arrow icon-angle-down"></b>
+						</a>
+
+						<ul class="submenu">
+							<li>
+								<a href="error-404.html">
+									<i class="icon-double-angle-right"></i>
+									Kategori Produk
+								</a>
+							</li>
+
+							<li>
+								<a href="error-500.html">
+									<i class="icon-double-angle-right"></i>
+									Merk Produk
+								</a>
+							</li>
+
+							<li>
+								<a href="grid.html">
+									<i class="icon-double-angle-right"></i>
+									Ukuran
+								</a>
+							</li>
+
+							<li class="">
+								<a href="blank.html">
+									<i class="icon-double-angle-right"></i>
+									Produk
+								</a>
+							</li>
+						</ul>
+					</li>
+				</ul>
+
+				<div class="sidebar-collapse" id="sidebar-collapse">
+					<i class="icon-double-angle-left"></i>
+				</div>
