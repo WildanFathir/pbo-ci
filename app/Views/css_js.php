2e18fd306fd140ae5c256439164f5541849e153c
WildanFathir <wildanfathir612@gmail.com>
2025-04-17 15:53:45 +0700

    setup project

--- app/Views/css_js.php
+++ app/Views/css_js.php
@@ -1,18 +1,18 @@
-<link href="http://pbo_wildan/assets/css/bootstrap.min.css" rel="stylesheet" />
-<link href="http://pbo_wildan/assets/css/bootstrap-responsive.min.css" rel="stylesheet" />
-<link rel="stylesheet" href="http://pbo_wildan/assets/css/font-awesome.min.css" />
-<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300" />
-<link rel="stylesheet" href="http://pbo_wildan/assets/css/ace.min.css" />
-<link rel="stylesheet" href="http://pbo_wildan/assets/css/ace-responsive.min.css" />
-<link rel="stylesheet" href="http://pbo_wildan/assets/css/ace-skins.min.css" />
+<link href="<?= base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet" />
+<link href="<?= base_url('assets/css/bootstrap-responsive.min.css') ?>" rel="stylesheet" />
+<link rel="stylesheet" href="<?= base_url('assets/css/font-awesome.min.css') ?>" />
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,300" />
+<link rel="stylesheet" href="<?= base_url('assets/css/ace.min.css') ?>" />
+<link rel="stylesheet" href="<?= base_url('assets/css/ace-responsive.min.css') ?>" />
+<link rel="stylesheet" href="<?= base_url('assets/css/ace-skins.min.css') ?>" />
 
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
 <script type="text/javascript">
-	window.jQuery || document.write("<script src='http://pbo_wildan/assets/js/jquery-2.0.3.min.js'>"+"<"+"/script>");
+	window.jQuery || document.write("<script src='<?= base_url('assets/js/jquery-2.0.3.min.js') ?>'>"+"<"+"/script>");
 </script>
 <script type="text/javascript">
-	if("ontouchend" in document) document.write("<script src='http://pbo_wildan/assets/js/jquery.mobile.custom.min.js'>"+"<"+"/script>");
+	if("ontouchend" in document) document.write("<script src='<?= base_url('assets/js/jquery.mobile.custom.min.js') ?>'>"+"<"+"/script>");
 </script>
-<script src="http://pbo_wildan/assets/js/bootstrap.min.js"></script>
-<script src="http://pbo_wildan/assets/js/ace-elements.min.js"></script>
-<script src="http://pbo_wildan/assets/js/ace.min.js"></script>
+<script src="<?= base_url('assets/js/bootstrap.min.js') ?>"></script>
+<script src="<?= base_url('assets/js/ace-elements.min.js') ?>"></script>
+<script src="<?= base_url('assets/js/ace.min.js') ?>"></script>
