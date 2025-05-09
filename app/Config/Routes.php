e46d9b8a375528264e6132ec4e911a8d274d4b9b
WildanFathir <wildanfathir612@gmail.com>
2025-05-09 20:42:15 +0700

    feat: define authentication routes for login, logout, and process actions

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Auth_controller::login');
+
+$routes->group('auth', function ($routes) {
+    $routes->get('login', 'Auth_controller::login');
+    $routes->get('logout', 'Auth_controller::logout');
+    $routes->post('proses', 'Auth_controller::proses');
+});
