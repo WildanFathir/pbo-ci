66cb22e94456cfd6480f03a7885c782f7a714592
WildanFathir <wildanfathir612@gmail.com>
2025-05-09 16:58:08 +0700

    feat: add Karyawan_model for employee data management and authentication

---
+++ app/Models/Karyawan_model.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Karyawan_model extends Model
+{
+    protected $table            = 'karyawan';
+    protected $primaryKey       = 'no_karyawan';
+
+    protected $allowedFields    = ['no_karyawan', 'nama_karyawan', 'alamat', 'password', 'foto'];
+
+    function cek_login($no_karyawan, $password)
+    {
+        $data = $this->where('no_karyawan', $no_karyawan)->first();
+
+        if ($data) {
+            if (password_verify($password, $data['password'])) {
+                return $data;
+            }
+        }
+
+        return null;
+    }
+}
