c382c5f184fb4d33adcfb4cf4900abdef02b9486
WildanFathir <wildanfathir612@gmail.com>
2025-05-09 16:58:20 +0700

    feat: implement login process with validation and session management in Auth_controller

--- app/Controllers/Auth_controller.php
+++ app/Controllers/Auth_controller.php
@@ -3,13 +3,73 @@
 namespace App\Controllers;
 
 use App\Controllers\Base_controller;
+use App\Models\Karyawan_model;
 
 class Auth_controller extends Base_controller
 {
     public function login()
     {
         $data['css_js'] = view('Css_js');
-
         return view('Login_view', $data);
+    }
+
+    public function proses()
+    {
+        if ($this->request->getPost('btn_login')) {
+            $no_karyawan = $this->request->getPost('no_karyawan');
+            $password = $this->request->getPost('password');
+            $M_karyawan = new Karyawan_model();
+            $proses = $M_karyawan->cek_login($no_karyawan, $password);
+
+            if (empty($no_karyawan) || empty($password)) {
+                session()->setFlashdata('info', '
+                    <div class="alert alert-danger">
+                        <button type="button" class="close" data-dismiss="alert">
+                            <i class="icon-remove"></i>
+                        </button>
+                        <strong>
+                            <i class="icon-remove"></i>
+                            Perhatian!
+                        </strong>
+                        No karyawan dan password harus diisi.
+                        <br>
+                    </div>
+                ');
+
+                return redirect()->to(base_url('Auth_controller/login'));
+            }
+
+            if ($proses) {
+                $data = [
+                    'status_login' => "True",
+                    'no_karyawan' => $proses['no_karyawan'],
+                    'nama_karyawan' => $proses['nama_karyawan'],
+                    'alamat' => $proses['alamat'],
+                    'password' => $proses['password'],
+                    'foto' => $proses['foto'],
+                ];
+
+                session()->set($data);
+                return redirect()->to(base_url('Dashboard_controller'));
+            } else {
+                session()->setFlashdata('info', '
+                    <div class="alert alert-danger">
+                        <button type="button" class="close" data-dismiss="alert">
+                            <i class="icon-remove"></i>
+                        </button>
+                        <strong>
+                            <i class="icon-remove"></i>
+                            Mohon Maaf!
+                        </strong>
+                        Data (No karyawan/password) tidak tepat.
+                        <br>
+                    </div>
+                ');
+
+                return redirect()->to(base_url('Auth_controller/login'));
+            }
+        }
+        
+        return redirect()->to(base_url('Auth_controller/login'));
     }
 }
--- app/Controllers/Dashboard_controller.php
+++ app/Controllers/Dashboard_controller.php
@@ -8,9 +8,21 @@ class Dashboard_controller extends Base_controller
 {
     public function index()
     {
-        $data['css_js'] = view('Css_js');
-        $data['navbar'] = view('Navbar_view');
-        $data['sidebar'] = view('Sidebar_view');
-        return view('Dashboard_view', $data);
+        $data_session = session();
+        if (!empty($data_session->get('status_login'))) {
+            $data['no_karyawan'] = $data_session->get('no_karyawan');
+            $data['nama_karyawan'] = $data_session->get('nama_karyawan');
+            $data['alamat'] = $data_session->get('alamat');
+            $data['password'] = $data_session->get('password');
+            $data['foto'] = $data_session->get('foto');
+
+            $data['css_js'] = view('Css_js');
+            $data['navbar'] = view('Navbar_view', $data);
+            $data['sidebar'] = view('Sidebar_view',);
+            return view('Dashboard_view', $data);
+        } else {
+            $data['css_js'] = view('Css_js');
+            return view('Login_view', $data);
+        }
     }
 }
