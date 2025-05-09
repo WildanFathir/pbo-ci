433810586d82c33942228f5b95412825dea0105f
WildanFathir <wildanfathir612@gmail.com>
2025-05-09 16:58:25 +0700

    feat: add session configuration comments in App.php for future reference

--- app/Config/App.php
+++ app/Config/App.php
@@ -199,4 +199,16 @@ class App extends BaseConfig
      * @see http://www.w3.org/TR/CSP/
      */
     public bool $CSPEnabled = false;
+    /**
+     * --------------------------------------------------------------------------
+     * Session Configuration
+     * --------------------------------------------------------------------------
+     */
+    // public $sessionDriver            = 'CodeIgniter\Session\Handlers\FileHandler';
+    // public $sessionCookieName       = 'ci_session';
+    // public $sessionExpiration       = 7200;
+    // public $sessionSavePath         = '/tmp/sessions';
+    // public $sessionMatchIP          = false;
+    // public $sessionTimeToUpdate     = 300;
+    // public $sessionRegenerateDestroy = false;
 }
