f3c739c68a4d9380cd814b8c49cb4a66af58fee8
WildanFathir <wildanfathir612@gmail.com>
2025-06-26 06:19:13 +0700

    fix: update routing configuration to include namespace for controllers in Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,19 +8,19 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Auth_controller::login');
 
 $routes->group('auth', function ($routes) {
-    $routes->get('login', 'Auth_controller::login');
-    $routes->get('logout', 'Auth_controller::logout');
-    $routes->post('proses', 'Auth_controller::proses');
+    $routes->get('login', 'auth\Auth_controller::login', ['as' => 'login']);
+    $routes->get('logout', 'auth\Auth_controller::logout', ['as' => 'logout']);
+    $routes->post('proses', 'auth\Auth_controller::proses', ['as' => 'prosesLogin']);
 });
 
 $routes->group('dashboard', function ($routes) {
     // main dashboard route
-    $routes->get('/', 'Dashboard_controller::index', ['as' => 'dashboard']);
+    $routes->get('/', 'dashboard\Dashboard_controller::index', ['as' => 'dashboard']);
 
     // karyawan routes
-    $routes->get('karyawan', 'Karyawan_controller::index', ['as' => 'karyawan']);
-    $routes->post('karyawan/simpan', 'Karyawan_controller::simpan', ['as' => 'simpanKaryawan']);
-    $routes->post('karyawan/edit', 'Karyawan_controller::ubah', ['as' => 'editKaryawan']);
-    $routes->get('karyawan/hapus/(:any)', 'Karyawan_controller::hapus/$1');
-    $routes->get('karyawan/cetak', 'Karyawan_controller::cetak', ['as' => 'cetakKaryawan']);
+    $routes->get('karyawan', 'dashboard\Karyawan_controller::index', ['as' => 'karyawan']);
+    $routes->post('karyawan/simpan', 'dashboard\Karyawan_controller::simpan', ['as' => 'simpanKaryawan']);
+    $routes->post('karyawan/edit', 'dashboard\Karyawan_controller::ubah', ['as' => 'editKaryawan']);
+    $routes->get('karyawan/hapus/(:any)', 'dashboard\Karyawan_controller::hapus/$1');
+    $routes->get('karyawan/cetak', 'dashboard\Karyawan_controller::cetak', ['as' => 'cetakKaryawan']);
 });
