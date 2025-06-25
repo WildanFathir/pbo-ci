41c3d2c4abd141e0f1cdcb8fd1ed6b80716158b8
WildanFathir <wildanfathir612@gmail.com>
2025-06-26 06:11:38 +0700

    fix: update routing configuration to disable auto routing and improve login view structure with route helper

--- app/Views/auth/Login_view.php
+++ app/Views/auth/Login_view.php
@@ -1,98 +1,100 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8" />
-		<title>Login - SITOKO v1.0</title>
 
-		<meta name="description" content="User login page" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<head>
+    <meta charset="utf-8" />
+    <title>Login - SITOKO v1.0</title>
 
-		<!--inline styles related to this page-->
-	    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="description" content="User login page" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-        <?php echo $css_js ?>
-    </head>
+    <!--inline styles related to this page-->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-	<body class="login-layout">
-		<div class="main-container container-fluid">
-			<div class="main-content">
-				<div class="row-fluid">
-					<div class="span12">
-						<div class="login-container">
-							<div class="row-fluid">
-								<div class="center">
-									<h1>
-										<i class="icon-leaf green"></i>
-										<span class="red">SITOKO</span>
-										<span class="white">v1.0</span>
-									</h1>
-									<h4 class="blue">&copy; Wildan Company</h4>
-								</div>
-							</div>
+    <?php echo $css_js ?>
+</head>
 
-							<div class="space-6"></div>
+<body class="login-layout">
+    <div class="main-container container-fluid">
+        <div class="main-content">
+            <div class="row-fluid">
+                <div class="span12">
+                    <div class="login-container">
+                        <div class="row-fluid">
+                            <div class="center">
+                                <h1>
+                                    <i class="icon-leaf green"></i>
+                                    <span class="red">SITOKO</span>
+                                    <span class="white">v1.0</span>
+                                </h1>
+                                <h4 class="blue">&copy; Wildan Company</h4>
+                            </div>
+                        </div>
 
-							<div class="row-fluid">
-								<div class="position-relative">
-									<div id="login-box" class="login-box visible widget-box no-border">
-										<div class="widget-body">
-											<div class="widget-main">
-												<h4 class="header blue lighter bigger">
-													<i class="icon-coffee green"></i>
-													Login
-												</h4>
+                        <div class="space-6"></div>
 
-												<div class="space-6"></div>
+                        <div class="row-fluid">
+                            <div class="position-relative">
+                                <div id="login-box" class="login-box visible widget-box no-border">
+                                    <div class="widget-body">
+                                        <div class="widget-main">
+                                            <h4 class="header blue lighter bigger">
+                                                <i class="icon-coffee green"></i>
+                                                Login
+                                            </h4>
 
-												<?php 
-												$session = session();
-												$info = $session->getFlashdata('info');
-												if ($info) {
-													echo $info;
-												}
-												?>
+                                            <div class="space-6"></div>
 
-												<form name="form_login" method="post" action="<?php echo base_url('Auth_controller/proses'); ?>">
-													<fieldset>
-														<label>
-															<span class="block input-icon input-icon-right">
-																<input type="text" name="no_karyawan" class="span12" placeholder="No Karyawan" />
-																<i class="icon-user"></i>
-															</span>
-														</label>
+                                            <?php
+                                            $session = session();
+                                            $info = $session->getFlashdata('info');
+                                            if ($info) {
+                                                echo $info;
+                                            }
+                                            ?>
 
-														<label>
-															<span class="block input-icon input-icon-right">
-																<input type="password" name="password" class="span12" placeholder="Password" />
-																<i class="icon-lock"></i>
-															</span>
-														</label>
+                                            <form name="form_login" method="post" action="<?= route_to('prosesLogin'); ?>">
+                                                <fieldset>
+                                                    <label>
+                                                        <span class="block input-icon input-icon-right">
+                                                            <input type="text" name="no_karyawan" class="span12" placeholder="No Karyawan" />
+                                                            <i class="icon-user"></i>
+                                                        </span>
+                                                    </label>
 
-														<div class="space"></div>
+                                                    <label>
+                                                        <span class="block input-icon input-icon-right">
+                                                            <input type="password" name="password" class="span12" placeholder="Password" />
+                                                            <i class="icon-lock"></i>
+                                                        </span>
+                                                    </label>
 
-														<div class="clearfix">
-															<button type="submit" name="btn_login" value="1" class="width-35 pull-right btn btn-small btn-primary">
-																<i class="icon-key"></i>
-																Login
-															</button>
-														</div>
+                                                    <div class="space"></div>
 
-														<div class="space-4"></div>
-													</fieldset>
-												</form>
-											</div><!--/widget-main-->
+                                                    <div class="clearfix">
+                                                        <button type="submit" name="btn_login" value="1" class="width-35 pull-right btn btn-small btn-primary">
+                                                            <i class="icon-key"></i>
+                                                            Login
+                                                        </button>
+                                                    </div>
 
-											<div class="toolbar clearfix center-white">
-                                                <h6 class="text-white text-center">&copy; Dibuat : Wildan Fathir Qinthara</h6>
-											</div>
-										</div><!--/widget-body-->
-									</div><!--/login-box-->
-								</div><!--/position-relative-->
-							</div>
-						</div>
-					</div><!--/.span-->
-				</div><!--/.row-fluid-->
-			</div>
-		</div><!--/.main-container-->
-	</body>
+                                                    <div class="space-4"></div>
+                                                </fieldset>
+                                            </form>
+                                        </div><!--/widget-main-->
+
+                                        <div class="toolbar clearfix center-white">
+                                            <h6 class="text-white text-center">&copy; Dibuat : Wildan Fathir Qinthara</h6>
+                                        </div>
+                                    </div><!--/widget-body-->
+                                </div><!--/login-box-->
+                            </div><!--/position-relative-->
+                        </div>
+                    </div>
+                </div><!--/.span-->
+            </div><!--/.row-fluid-->
+        </div>
+    </div><!--/.main-container-->
+</body>
+
 </html>
--- app/Views/components/Navbar_view.php
+++ app/Views/components/Navbar_view.php
@@ -1,49 +1,49 @@
 <div class="navbar-inner">
-				<div class="container-fluid">
-					<a href="#" class="brand">
-						<small>
-							<i class="icon-leaf"></i>
-							SISTEM INFORMASI TOKO
-						</small>
-					</a><!--/.brand-->
+    <div class="container-fluid">
+        <a href="#" class="brand">
+            <small>
+                <i class="icon-leaf"></i>
+                SISTEM INFORMASI TOKO
+            </small>
+        </a><!--/.brand-->
 
-					<ul class="nav ace-nav pull-right">
-						<li class="light-blue">
-							<a data-toggle="dropdown" href="#" class="dropdown-toggle">
-								<img class="nav-user-photo" src="<?php echo base_url();?>/assets/avatars/<?php echo $foto ?>" alt="<?php echo $foto ?>" />
-								<span class="user-info">
-									<small>Selamat Datang</small>
-									<?php echo $nama_karyawan ?>
-								</span>
+        <ul class="nav ace-nav pull-right">
+            <li class="light-blue">
+                <a data-toggle="dropdown" href="#" class="dropdown-toggle">
+                    <img class="nav-user-photo" src="<?php echo base_url(); ?>/assets/avatars/<?php echo $foto ?>" alt="<?php echo $foto ?>" />
+                    <span class="user-info">
+                        <small>Selamat Datang</small>
+                        <?php echo $nama_karyawan ?>
+                    </span>
 
-								<i class="icon-caret-down"></i>
-							</a>
+                    <i class="icon-caret-down"></i>
+                </a>
 
-							<ul class="user-menu pull-right dropdown-menu dropdown-yellow dropdown-caret dropdown-closer">
-								<li>
-									<a href="#">
-										<i class="icon-cog"></i>
-										Settings
-									</a>
-								</li>
+                <ul class="user-menu pull-right dropdown-menu dropdown-yellow dropdown-caret dropdown-closer">
+                    <li>
+                        <a href="#">
+                            <i class="icon-cog"></i>
+                            Settings
+                        </a>
+                    </li>
 
-								<li>
-									<a href="#">
-										<i class="icon-user"></i>
-										Profile
-									</a>
-								</li>
+                    <li>
+                        <a href="#">
+                            <i class="icon-user"></i>
+                            Profile
+                        </a>
+                    </li>
 
-								<li class="divider"></li>
+                    <li class="divider"></li>
 
-								<li>
-									<a href="<?php echo base_url('auth/logout') ?>">
-										<i class="icon-off"></i>
-										Logout
-									</a>
-								</li>
-							</ul>
-						</li>
-					</ul>
-				</div>
-			</div>
+                    <li>
+                        <a href="<?= route_to('logout') ?>">
+                            <i class="icon-off"></i>
+                            Logout
+                        </a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div>
