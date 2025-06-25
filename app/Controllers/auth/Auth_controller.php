34a50335a209f6f92996c00fe14f1f48271d395f
WildanFathir <wildanfathir612@gmail.com>
2025-06-26 06:18:57 +0700

    fix: update redirect paths in Auth_controller to use simplified route for dashboard

--- app/Controllers/auth/Auth_controller.php
+++ app/Controllers/auth/Auth_controller.php
@@ -11,7 +11,7 @@ class Auth_controller extends Base_controller
     {
         $sessiopn = session();
         if (!empty($sessiopn->get('status_login'))) {
-            return redirect()->to(base_url('Dashboard_controller'));
+            return redirect()->to('dashboard');
         } else {
             $data['css_js'] = view('components/Css_js');
             return view('auth/Login_view', $data);
@@ -55,7 +55,7 @@ class Auth_controller extends Base_controller
                 ];
 
                 session()->set($data);
-                return redirect()->to(base_url('Dashboard_controller'));
+                return redirect()->to('dashboard');
             } else {
                 session()->setFlashdata('info', '
                     <div class="alert alert-danger">
