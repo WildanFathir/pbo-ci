35e2029721fc2f1e1e9dbe713f5fac071716162a
WildanFathir <wildanfathir612@gmail.com>
2025-07-17 11:23:40 +0700

    fix: update page titles to "Toko Wildan" across multiple views

--- app/Views/auth/Login_view.php
+++ app/Views/auth/Login_view.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>Login - SITOKO v1.0</title>
+    <title>Login - Toko Wildan</title>
 
     <meta name="description" content="User login page" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
